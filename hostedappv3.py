0f905bae82bb36baf30fd8da4c252de871d48841
zpalmer <zoepalmer2014@u.northwestern.edu>
2016-04-11 21:06:42 -0500

    added self-signed SSL cert using only TLSv1.2 to test PayPal

--- hostedappv3.py
+++ hostedappv3.py
@@ -1,12 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 import braintree
+import ssl
 
 app = Flask(__name__)
-
-# from OpenSSL import ssl
-# tls_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-# context.load_cert_chain('yourserver.crt', 'yourserver.key')
-tls_context = "adhoc"
 
 merchant_id = "ryqy4yyw7m5bf92h"
 braintree.Configuration.configure(
@@ -15,6 +11,13 @@ braintree.Configuration.configure(
     public_key = "ymtqgy8773zq2fw3",
     private_key = "7dd7253c4c53d675f15e869212659579"
 )
+
+# self_signed_ssl_cert_path = url_for("static", filename="server.crt")
+# self_signed_ssl_cert_key_path = url_for("static", filename="server.key")
+self_signed_ssl_cert_path = "static/server.crt"
+self_signed_ssl_cert_key_path = "static/server.key"
+tls_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+tls_context.load_cert_chain(self_signed_ssl_cert_path, self_signed_ssl_cert_key_path)
 
 @app.route('/', methods=["GET"])
 def get_client_token():
@@ -47,7 +50,5 @@ def create_transaction():
 	# return "Victory! We got a nonce (" + nonce + ") and an amount (" + amount + ")!"
 
 if __name__ == '__main__':
-	app.debug = True
-	app.ssl_context = tls_context
-	# app.run()
-	app.run('127.0.0.1', debug=True, port=5000, ssl_context='adhoc')
+	
+	app.run('127.0.0.1', debug=True, port=5000, ssl_context=tls_context)
