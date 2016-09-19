5d55b2013b917727863c026fc7dd7ddb32add9eb
zpalmer <zoepalmer2014@u.northwestern.edu>
2016-09-19 17:01:01 -0500

    Adds 3D S. Updates to 3.2.0. Has not been extensively tested.

--- hostedappv3.py
+++ hostedappv3.py
@@ -52,6 +52,6 @@ def create_transaction():
 
 
 if __name__ == '__main__':
-#	commenting out the run statement with the SSL context so as to not register as insecure.
-#	app.run('127.0.0.1', debug=True, port=5000, ssl_context=tls_context)
+#	comment out the run statement with the SSL context so as to not register as insecure.
+	#app.run('127.0.0.1', debug=True, port=5000, ssl_context=tls_context)
 	app.run('127.0.0.1', debug=True, port=5000)
